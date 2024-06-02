45a3314c5ef625b6425107b0044aed73be02a126
Wellington <diaswellin1@gmail.com>
2024-06-01 23:48:41 -0300

    adding second test route

--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,14 +1,20 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteApiController {
 	
 	@GetMapping("/teste-api")
-	private String teste() {
+	public String teste() {
 		return "Sucesso";
+	}
+	
+	@GetMapping("/teste-api-bem-vindo")
+	public String testeBemVindo(@RequestParam(name = "nome") String nome) {
+		return "Olá, " + nome + ". Seja bem vindo!";
 	}
 
 }
